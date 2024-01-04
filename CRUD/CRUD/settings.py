3c4b42518079c104c64b77b6256aae21042629f2
Gaikwad-Vinayak <gaikwadvinayak924@gmail.com>
2024-01-04 15:02:37 +0530

    set allowed host

--- CRUD/CRUD/settings.py
+++ CRUD/CRUD/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-3y=n!a$4j2%ee=!7ieqh4a=&#tkcl@nn9ntx0zn6n(c%&i@+od
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
